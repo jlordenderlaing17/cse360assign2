8294e3d7111538262e156e33f0435e95f24c34c0
jlordenderlaing17 <39042679+jlordenderlaing17@users.noreply.github.com>
2019-02-20 19:45:05 -0700

    Initial commit

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,83 @@
+/**
+Name: James Lord Ender Laing
+Class ID: 460
+Assignment 2
+This file is the class for a Calculator object along with its methods
+*/
+
+package cse360assign2;
+
+/**
+ * This class' purpose is to perform operations relative to the one instance variable, total.
+ * This class is not yet properly implemented. It has a default constructor that sets total equal to zero.
+ * This class contains six methods that are all not properly implemented.
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+/**
+ * Returns 0
+ * <p>
+ * This method is not properly implemented yet.
+ * @return	0
+ */
+	public int getTotal () {
+		return 0;
+	}
+	
+/**
+ * Does nothing.
+ * <p>
+ * This method is not properly implemented yet.
+ * @param value an integer that does nothing in this version of the code.
+ */
+	public void add (int value) {
+		
+	}
+
+/**
+ * 	Does nothing.
+ * <p>
+ * This method is not properly implemented yet.
+ * @param value an integer that does nothing in this version of the code
+ */
+	public void subtract (int value) {
+		
+	}
+	
+/**
+ * Does nothing.
+ * <p>
+ * This method is not properly implemented yet.
+ * @param value an integer that does nothing in this version of the code
+ */
+	public void multiply (int value) {
+		
+	}
+	
+/**
+ * Does nothing.
+ * <p>
+ * This method is not properly implemented yet.
+ * @param value an integer that does nothing in this version of the code.
+ */
+	public void divide (int value) {
+		
+	}
+	
+/**
+ * Returns an empty string.
+ * <p>
+ * This method is not properly implemented yet.
+ * @return an empty string.
+ */
+	public String getHistory () {
+		return "";
+	}
+}
