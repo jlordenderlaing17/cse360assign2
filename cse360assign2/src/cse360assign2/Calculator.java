14dcee53e4b2dbbf127775de506205dbfdd84611
jlordenderlaing17 <39042679+jlordenderlaing17@users.noreply.github.com>
2019-02-21 19:55:03 -0700

    Second Version.
    This version implements the changes demanded by the second part of the
    assignment.

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -9,72 +9,73 @@ package cse360assign2;
 
 /**
  * This class' purpose is to perform operations relative to the one instance variable, total.
- * This class is not yet properly implemented. It has a default constructor that sets total equal to zero.
- * This class contains six methods that are all not properly implemented.
+ * This class is not yet completely implemented. It has a default constructor that sets total equal to zero.
+ * The operations this class can handle are addition, subtraction, multiplication, and division.
+ * It can also return the total instance variable.
  */
 
 public class Calculator {
 
-	private int total;
+	private int total; // This is the instance variable that can be altered by some of the following methods.
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 /**
- * Returns 0
- * <p>
- * This method is not properly implemented yet.
- * @return	0
+ * Returns the total.
+ * @return	the instance variable, total
  */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 /**
- * Does nothing.
- * <p>
- * This method is not properly implemented yet.
- * @param value an integer that does nothing in this version of the code.
+ * Adds a number to the total instance variable, and assigns the sum to total.
+ * @param value an integer that is added to the total.
  */
 	public void add (int value) {
-		
+		total = total + value;
 	}
 
 /**
- * 	Does nothing.
- * <p>
- * This method is not properly implemented yet.
- * @param value an integer that does nothing in this version of the code
+ * Subtracts a number from the total instance variable, and assigns the difference to total.
+ * @param value an integer that is subtracted from the total instance variable
  */
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
 	
 /**
- * Does nothing.
- * <p>
- * This method is not properly implemented yet.
- * @param value an integer that does nothing in this version of the code
+ * Multiplies a number by the total instance variable, and then assigns the product to
+ * total.
+ * @param value an integer that is multiplied by the total instance variable
  */
 	public void multiply (int value) {
-		
+		total = total * value;
 	}
 	
 /**
- * Does nothing.
+ * Divides the total instance variable by a number, and then assigns that quotient to total.
  * <p>
+ * If the number total is divided by is zero, total is just set to zero.
  * This method is not properly implemented yet.
- * @param value an integer that does nothing in this version of the code.
+ * @param value an integer that divides the total instance variable.
  */
 	public void divide (int value) {
-		
+		if(value == 0)
+		{
+			total = 0;
+		}
+		else
+		{
+			int temporaryQuotient = total/value;
+			total = temporaryQuotient;
+		}
 	}
 	
 /**
  * Returns an empty string.
- * <p>
- * This method is not properly implemented yet.
  * @return an empty string.
  */
 	public String getHistory () {
